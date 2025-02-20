4a80184fd797e9a6f822e2f12b7a611333a38a4e
Simon <2445042+Googlproxer@users.noreply.github.com>
2025-02-20 11:59:20 +1100

    Fixed ios build errors.

--- ios/Sources/BackgroundBLEPlugin/BackgroundBLEPlugin.swift
+++ ios/Sources/BackgroundBLEPlugin/BackgroundBLEPlugin.swift
@@ -10,10 +10,15 @@ public class BackgroundBLEPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "BackgroundBLEPlugin"
     public let jsName = "BackgroundBLE"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "addDevice", returnType: CAPPluginReturnPromise)
-        CAPPluginMethod(name: "removeDevice", returnType: CAPPluginReturnPromise)
-        CAPPluginMethod(name: "startForegroundService", returnType: CAPPluginReturnPromise)
-        CAPPluginMethod(name: "stopForegroundService", returnType: CAPPluginReturnPromise)
+        CAPPluginMethod(name: "initialise", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "addDevice", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "addDevices", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "removeDevice", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "startForegroundService", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "stopForegroundService", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "isRunning", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "getDevices", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "setScanMode", returnType: CAPPluginReturnPromise),
     ]
     private let implementation = BackgroundBLE()
 
@@ -22,6 +27,10 @@ public class BackgroundBLEPlugin: CAPPlugin, CAPBridgedPlugin {
     }
 
     @objc public func addDevice(_ call: CAPPluginCall) {
+        call.unimplemented("Not implemented on iOS")
+    }
+
+    @objc public func addDevices(_ call: CAPPluginCall) {
         call.unimplemented("Not implemented on iOS")
     }
 
@@ -38,6 +47,14 @@ public class BackgroundBLEPlugin: CAPPlugin, CAPBridgedPlugin {
     }
 
     @objc public func isRunning(_ call: CAPPluginCall) {
+        call.unimplemented("Not implemented on iOS")
+    }
+
+    @objc public func getDevices(_ call: CAPPluginCall) {
+        call.unimplemented("Not implemented on iOS")
+    }
+
+    @objc public func setScanMode(_ call: CAPPluginCall) {
         call.unimplemented("Not implemented on iOS")
     }
 
